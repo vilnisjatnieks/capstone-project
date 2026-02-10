2ecce2e98792307f4d018bad3e6df470641f2752
Aidan Marshall <armarsh1023@gmail.com>
2026-02-09 20:40:19 -0500

    Revert "Merge pull request #15 from vilnisjatnieks/U31-login-logout"
    This reverts commit 4ce9c7bf9b4d3c0e9f49a74fbaec7d70539dd68a, reversing
    changes made to 1c9a6cb0a819877884506fa866ca816a4fee1408.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="mx-auto max-w-7xl px-4 py-8">
+    <div className="flex min-h-screen items-center justify-center">
       <h1 className="text-4xl font-bold">Hello World</h1>
     </div>
   );
